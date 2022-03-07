17606a7aeb4d888e8c04909f0cd687db4c80bcd7
Alex <chugun777akk@mail.ru>
2022-03-07 11:09:00 +0700

    task2 is done

---
+++ Hw2task2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Показать четные числа от 1 до N
+Console.WriteLine("Введите число до которого показывать четные числа : ");
+int num = int.Parse(Console.ReadLine());
+for (int i = 1; i <= num; i++)
+{
+    if(i%2==0)
+    {
+        Console.Write(i + " ");
+    }
+}
